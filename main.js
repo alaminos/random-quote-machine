6ed3e19f8270c1a13aca239fcd30920550145893
Antonio Alaminos <aalaminosz@gmail.com>
2018-03-22 19:09:29 +0100

    more quotes added

--- main.js
+++ main.js
@@ -5,12 +5,27 @@ core = {
         "Je sens que je suis libre mais je sais que je ne le suis pas",
         "Se méfier des penseurs dont l'esprit ne fonctionne qu'à partir d'une citation",
         "Un homme qui se respecte n'a pas de patrie. La patrie c'est de la glu",
-        "Une opinion, oui. Une conviction, non. Tel est le point de la fierté intelectuelle"
+        "Une opinion, oui. Une conviction, non. Tel est le point de la fierté intelectuelle",
+        "Tout désespoir est un ultimatum à Dieu.",
+        "Apprenez à aimer les attitudes injustifiées, les gestes inexplicables, les actions sans mobile !",
+        "Une larme a toujours des sources plus profondes qu'un sourire.",
+        "L'acte absurde est l'expression la plus haute de la liberté.",
+        "L'indifférence est un crime envers la vie et envers la souffrance.",
+        "Que la folie soit notre seule sagesse ; que toute notre vie soit un élan qui nous brûle d'une fièvre.",
+        "Qui parle au nom des autres est toujours un imposteur.",
+        "Plus les hommes s'éloignent de Dieu, plus ils avancent dans la connaissance des religions.",
+        "On n’habite pas un pays, on habite une langue.",
+        "Quel dommage que, pour aller à Dieu, il faille passer par la foi !",
+        "Le scepticisme est l'élégance de l'anxiété.",
+        "Ce n’est pas la peine de se tuer puisqu’on se tue toujours trop tard.",
+        "Sur le plan spirituel, toute douleur est une chance ; sur le plan spirituel seulement.",
+        "Je donnerais tous les paysages du monde pour celui de mon enfance.",
+        "On meurt depuis toujours et cependant la mort n'a rien perdu de sa fraîcheur."
     ],
 
     randomNum: 0,
 
-    //para evitar que la misma cita sea elegida, debemos mantener el registro y prohibirla en la siguiente generación de random quote
+ 
 
     quotePicker: function() {
         var newRandomNum = Math.floor(Math.random() * (this.quotes.length));
